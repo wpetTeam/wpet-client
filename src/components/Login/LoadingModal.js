2473cb5de6b8725065c39feaba64d4b32be68d99
seeun-lim <ssen.lim@gmail.com>
2022-04-28 19:59:37 +0900

    Feat: 추가 로그인 로딩 화면

--- src/components/Login/LoadingModal.js
+++ src/components/Login/LoadingModal.js
@@ -3,23 +3,53 @@ import {
     LoadingContainer,
     LoadingPicture,
     ModalFrame,
+    Text,
 } from 'assets/styles/Login/style';
 import 'assets/styles/Login/_style.scss';
 import React, { useState } from 'react';
+import { useInterval } from 'utils';
 import Logo from 'assets/images/Logo/text-icon.png';
 
 const LoadingModal = () => {
+    const [showProfile, setShowProfile] = useState(false);
+    const [userName, setUserName] = useState('');
+    /* 추후에 이미지 fetch 할 예정  */
     const [profile, setProfile] = useState(
-        'https://github.com/wpetTeam/wpet-client/blob/feature/src/assets/images/sample.jpg',
+        'https://raw.githubusercontent.com/wpetTeam/wpet-client/feature/src/assets/images/sample.jpg',
     );
+    const delay = 2000;
+
+    useInterval(() => {
+        setShowProfile(true);
+        setUserName('미남누나');
+    }, delay);
+
     return (
         <LoadingContainer>
-            <ModalFrame>
+            <ModalFrame className="loading">
                 <BoldText className="thank-text">Welcome Back!</BoldText>
                 <LoadingPicture>
-                    {/* <img className="loading-image" src={Logo} alt="로고" /> */}
-                    <img src={profile} alt="유저 프로필" width={180} />
+                    {!showProfile && (
+                        <img className="loading-image" src={Logo} alt="로고" />
+                    )}
+                    {showProfile && (
+                        <img
+                            className="profile-image"
+                            src={profile}
+                            alt="유저 프로필"
+                            width={180}
+                        />
+                    )}
                 </LoadingPicture>
+                {!showProfile && <Text>로딩 중입니다...</Text>}
+                {showProfile && (
+                    <Text>
+                        <span>
+                            <b>{userName}</b>
+                        </span>
+                        {'  '}님
+                    </Text>
+                )}
             </ModalFrame>
         </LoadingContainer>
     );
