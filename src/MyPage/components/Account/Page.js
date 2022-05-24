c091f70e4c1f1eb02d0b66797ca8ff36b254bc45
seeun-lim <ssen.lim@gmail.com>
2022-05-24 21:20:25 +0900

    Feat: 나의 계정 요소 추가

--- src/MyPage/components/Account/Page.js
+++ src/MyPage/components/Account/Page.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Icon } from '@iconify/react';
-import { API } from 'utils';
+import logo from 'assets/images/Logo/text-icon.png';
+import { API, removePicture, uploadPicture } from 'utils';
 import './styles/_style.scss';
 
 const MyAccount = () => {
     const [user, setUser] = useState({});
+    const [updateInfo, setUpdateInfo] = useState({});
+    const [profilePic, setProfilePic] = useState('');
+    const [updateEmail, setUpdateEmail] = useState(false);
+    const [updateProfile, setUpdateProfile] = useState(false);
+
     useEffect(() => {
         const getAuth = async () => {
             await API.get('/user/auth', {
@@ -15,6 +21,8 @@ const MyAccount = () => {
                     console.log('>>> [ACCOUNT] ✅ SUCCESS', res.data);
                     if (res.status === 200) {
                         setUser(res.data);
+                        setUpdateInfo(res.data);
+                        setProfilePic(res.data.profilePicture);
                     }
                 })
                 .catch((err) =>
@@ -23,58 +31,43 @@ const MyAccount = () => {
         };
         getAuth();
     }, []);
-    const userInfo = {
-        nickName: '하하',
-        email: 'abc@wpet.com',
-        joinDate: '2022-05-09',
-        profilePicture: '',
+
+    const handleUpdateInfo = (e) => {
+        setUpdateInfo({ ...updateInfo, [e.target.name]: e.target.value });
     };
-    const [updateEmail, setUpdateEmail] = useState(false);
-    const [updateInfo, setUpdateInfo] = useState(false);
+
     return (
         <Component className="myaccount-component">
             <div className="first-row">
                 <EmailBox
-                    userInfo={user}
+                    info={updateInfo}
                     updateEmail={updateEmail}
                     setUpdateEmail={setUpdateEmail}
+                    handleUpdateInfo={handleUpdateInfo}
                 />
                 <PwBox />
+                <DateBox info={updateInfo} />
             </div>
             <div className="second-row">
-                <div className="profile-box">
-                    <img
-                        className="user-picture"
-                        src={user.profilePicture}
-                        alt="유저 프로필"
-                        width={300}
+                <ProfileBox
+                    info={updateInfo}
+                    profilePic={profilePic}
+                    setProfilePic={setProfilePic}
+                    updateProfile={updateProfile}
+                    setUpdateProfile={setUpdateProfile}
+                    handleUpdateInfo={handleUpdateInfo}
+                />
+            </div>
+            <div className="third-row">
+                <div className="pet-box">미남이</div>
+                <div className="community-box">
+                    <Icon
+                        className="icon no"
+                        icon="teenyicons:message-no-access-solid"
                     />
-                    <div className="name-box">
-                        <input
-                            className="name-input"
-                            name="nickName"
-                            value={user.nickName}
-                            disabled={updateInfo ? false : true}
-                        />
-                        {updateInfo ? (
-                            <button
-                                onClick={() => setUpdateInfo(false)}
-                                className="update-btn"
-                            >
-                                수정 완료
-                            </button>
-                        ) : (
-                            <button
-                                onClick={() => setUpdateInfo(true)}
-                                className="update-btn"
-                            >
-                                정보 수정
-                            </button>
-                        )}
-                    </div>
+                    커뮤니티에 가입하지 않았습니다.
                 </div>
             </div>
-            <Date>{userInfo.joinDate}</Date>
         </Component>
     );
 };
@@ -83,18 +76,13 @@ export default MyAccount;
 const Component = styled.div`
     width: 100%;
     height: 93%;
-
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
 `;
 
-const Date = styled.div`
-    width: 20%;
-`;
-
-function EmailBox({ userInfo, updateEmail, setUpdateEmail }) {
+function EmailBox({ info, updateEmail, setUpdateEmail, handleUpdateInfo }) {
     return (
         <div className="email-box">
             {updateEmail ? (
@@ -114,12 +102,13 @@ function EmailBox({ userInfo, updateEmail, setUpdateEmail }) {
                         updateEmail ? 'email-input update' : 'email-input'
                     }
                     name="email"
-                    value={userInfo.email}
+                    value={info.email}
+                    onChange={handleUpdateInfo}
                     disabled={updateEmail ? false : true}
                 />
                 {updateEmail ? (
                     <button
-                        className="update-btn"
+                        className="update-btn auth"
                         onClick={() => setUpdateEmail(false)}
                     >
                         인증
@@ -151,10 +140,95 @@ function EmailBox({ userInfo, updateEmail, setUpdateEmail }) {
 function PwBox({}) {
     return (
         <div className="pw-box">
+            <Icon className="icon" icon="teenyicons:password-solid" />
             <p>비밀번호 변경을 원하시나요?</p>
             <div className="btn-box">
-                <button className="update-btn update">변경하기</button>
+                <button className="update-btn update">
+                    네, 변경하고 싶습니다.
+                </button>
             </div>
+        </div>
+    );
+}
+
+function ProfileBox({
+    info,
+    profilePic,
+    setProfilePic,
+    updateProfile,
+    setUpdateProfile,
+    handleUpdateInfo,
+}) {
+    var inputRef;
+    return (
+        <div className="profile-box">
+            <div className="pic-box">
+                <img
+                    className={updateProfile ? 'user-pic update' : 'user-pic'}
+                    src={profilePic === '' ? logo : profilePic}
+                    alt="유저 프로필"
+                />
+                {updateProfile && (
+                    <div className="btn-container">
+                        <button
+                            className="btn delete"
+                            onClick={(e) => removePicture(e, setProfilePic)}
+                        >
+                            삭제
+                        </button>
+                        <button
+                            className="btn reselect"
+                            onClick={() => inputRef.click()}
+                        >
+                            재선택
+                        </button>
+                        <input
+                            type="file"
+                            accept="image/*"
+                            name="profile_img"
+                            onChange={(e) => uploadPicture(e, setProfilePic)}
+                            ref={(refParam) => (inputRef = refParam)}
+                            hidden={true}
+                        />
+                    </div>
+                )}
+            </div>
+            <div className="name-box">
+                <input
+                    className={
+                        updateProfile ? 'name-input update' : 'name-input'
+                    }
+                    name="nickName"
+                    value={info.nickName}
+                    onChange={handleUpdateInfo}
+                    disabled={updateProfile ? false : true}
+                    placeholder="닉네임"
+                />
+            </div>
+            {updateProfile ? (
+                <button
+                    onClick={() => setUpdateProfile(false)}
+                    className="update-btn"
+                >
+                    수정 완료
+                </button>
+            ) : (
+                <button
+                    onClick={() => setUpdateProfile(true)}
+                    className="update-btn"
+                >
+                    정보 수정
+                </button>
+            )}
+        </div>
+    );
+}
+
+function DateBox({ info }) {
+    return (
+        <div className="date-box">
+            가입날짜
+            <p>{info.joinDate}</p>
         </div>
     );
 }
